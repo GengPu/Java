84439ac51f0336c9169ef79992c2f64381178fec
GengPu <GengPu@users.noreply.github.com>
2018-10-12 20:18:58 +0800

    系统参数

---
+++ Example/src/SystemExample/SystemExample.java
@@ -0,0 +1,17 @@
+package SystemExample;
+
+import java.util.Properties;
+
+public class SystemExample {
+
+	public static void main(String[] args) {
+		Properties p = System.getProperties();
+		p.list(System.out);
+		
+//		System.getProperty();
+	}
+	public void printAllProperty() {
+		
+		
+	}
+}
