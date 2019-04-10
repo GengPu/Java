26a3568f010992c1ad91173eaf5fe80958a5ba61
GengPu <virgil_lgp@163.com>
2019-04-10 16:28:58 +0800

    转化为maven项目

--- Example/src/JsonExample/JsonExample.java
+++ Example/src/JsonExample/JsonExample.java
@@ -1,6 +1,8 @@
 package JsonExample;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -15,9 +17,9 @@ public class JsonExample {
 	 */
 	public static JSONObject checkAndPutNull(JSONObject json){
 	
-		Set<String> keys = json.keySet();
-		List<Object> list = new ArrayList<Object>();
-		String str = "";
+		Set<String> keys = json.keySet();//获取json中key的列表
+		List<Object> list = new ArrayList<Object>();//创建空的列表
+		String str = "";//创建空的字符串
 		for (String key : keys) {
 			if(("attr".equals(key) || "prop".equals(key)) /* 这里需要控制这个key不是那种数组的key 比如："a":"123" */ && json.get(key) == null) {
 				json.put(key, str);
@@ -31,11 +33,13 @@ public class JsonExample {
 	public static void main(String[] args) {
 		
 		String jsonstr = "{\"attr\":null , \"list\":null , \"prop\":\"123\"}";
-		
+		Calendar c1 = Calendar.getInstance();
 		JSONObject json = JSONObject.parseObject(jsonstr);
 		System.out.println("转花前：\t"+json.toString());
 		json = checkAndPutNull(json);
 		System.out.println("转化后：\t"+json.toString());
+		Calendar c2 = Calendar.getInstance();
+		System.out.println(c1.compareTo(c2)+"----");
 
 		
 	}
