e7466ace03b511a72a756b80a45e82a39c5fd400
GengPu <virgil_lgp@163.com>
2019-06-03 14:32:38 +0800

    设计模式相关例子实例编写

--- Example/src/Example/Example.java
+++ Example/src/Example/Example.java
@@ -1,5 +1,7 @@
 package Example;
 
+import java.text.DecimalFormat;
+
 public class Example {
 	public static void main(String[] args) {
 //		MapExample exp = new MapExample();
@@ -18,7 +20,20 @@ public class Example {
 //		System.out.println(dexp.toString());
 //		MapExample.staticsetValue(dexp);
 //		System.out.println(dexp.toString());
+		
+		Double FSalePrice = null;
+		DecimalFormat numberFormat = new DecimalFormat("#,##0.00");//定义格式化输出
+		String formatstr = "";//定义返回结果
+//处理0 和 小于数值1的问题(例如:0.05会转为.05) jdk1.10没有这个问题
+//		if(FSalePrice.compareTo(Double.valueOf(1)) < 0){
+//		    numberFormat = new DecimalFormat("#,##0.00");
+//		}
+//		formatstr = FSalePrice.compareTo(Double.valueOf(0)) == 0?"0.00":numberFormat.format(FSalePrice == null ? 0.0:FSalePrice);
+		formatstr = numberFormat.format(FSalePrice == null ? 0.0:FSalePrice);
+		System.out.println(formatstr);
 	}
+	
+	
 	
 	 void go(){
 		System.out.println("Example");
